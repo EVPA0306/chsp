74f2cba2f972de7a565de7589841a46f40804b8c
EVPA0306 <pavlenko.evgeny@gmail.com>
2017-01-18 14:35:07 -0700

    Minor updates

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -4,6 +4,6 @@
     angular
         .module('chspApp')
         .constant('VERSION', "0.0.1")
-        .constant('DEBUG_INFO_ENABLED', false)
+        .constant('DEBUG_INFO_ENABLED', true)
 ;
 })();
